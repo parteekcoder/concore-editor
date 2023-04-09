c08feb59d65e9beaa0b1c45b064067d71ee7ba9b
Rahul <rahuljagwani1012@gmail.com>
2023-04-09 21:37:59 +0530

    restore icon changed

--- src/toolbarActions/toolbarList.js
+++ src/toolbarActions/toolbarList.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-alert */
 import {
     FaSave, FaUndo, FaRedo, FaTrash, FaFileImport, FaPlus, FaDownload, FaEdit, FaRegTimesCircle, FaHistory,
-    FaHammer, FaBug, FaBomb, FaToggleOn, FaThermometerEmpty, FaUndoAlt,
+    FaHammer, FaBug, FaBomb, FaToggleOn, FaThermometerEmpty,
 } from 'react-icons/fa';
+
+import {
+    BsArrowRepeat,
+} from 'react-icons/bs';
 
 import {
     // FiChevronDown, FiChevronsDown, FiChevronsUp, FiChevronUp,
@@ -72,7 +76,7 @@ const toolbarList = (state, dispatcher) => [
     {
         type: 'action',
         text: 'Restore',
-        icon: FaUndoAlt,
+        icon: BsArrowRepeat,
         action: resetAfterClear,
         active: state.curGraphInstance && state.resetEnabled,
         visibility: true,
